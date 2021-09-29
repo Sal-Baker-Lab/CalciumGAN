031966fd9c1cc635de6d3eb14ec79c1b5373f40c
Hussein Moghnieh <hussein@gofastpay.com>
2021-09-28 17:38:23 -0700

    GPU fix

--- predict.py
+++ predict.py
@@ -32,6 +32,17 @@ warnings.filterwarnings('ignore')
 # import keras.backend.tensorflow_backend as tb
 # tb._SYMBOLIC_SCOPE.value = True
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
 
 dirname = os.path.dirname(__file__)
 
