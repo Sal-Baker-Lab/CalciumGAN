8c32a4cb634f32240be222a3cb526103badf63f9
Hussein Moghnieh <hussein.moghnieh@gmail.com>
2021-10-19 11:55:27 -0700

    Plot size

--- stats.py
+++ stats.py
@@ -5,6 +5,11 @@ import altair as alt
 import random
 import numpy
 
+PLOT_W=2
+PLOT_H=5
+PLOT_TITLE_FONT_S=12
+PLOT_LABEL_FONT_S=10
+
 def stats(global_quant_df):
     stats_df = global_quant_df.groupby(by=["Image"]).agg(['mean', 'count'])
     stats_df = stats_df.reset_index()
@@ -18,15 +23,15 @@ def stats(global_quant_df):
 def generate_frequency_plot(df, file_name=None):
     df['category'] = numpy.random.uniform(0, 1, len(df))
     fig, ax = plt.subplots()
-    ax.figure.set_size_inches(1, 3)
+    ax.figure.set_size_inches(PLOT_W, PLOT_H)
 
     g = sns.swarmplot(x='category', y='Frequency_count', data=df, dodge=True, palette='viridis', ax=ax)
     sns.despine(fig=None, ax=None, top=True, right=True, left=False, bottom=False, offset=None, trim=False)
 
     ax.set_xlabel('')
     g.set(xticks=[])
-    ax.set_ylabel(r'Frequency No. of ' + r'$Ca^2+ Events$' +'\n (per STMap)', fontsize = 18)
-    plt.title("Events", fontsize=18)
+    ax.set_ylabel(r'Frequency No. of ' + r'$Ca^2+ Events$' +'\n (per STMap)', fontsize = PLOT_LABEL_FONT_S)
+    plt.title("Events", fontsize=PLOT_TITLE_FONT_S)
     if file_name is not None:
         plt.savefig(file_name)
     else:
@@ -35,15 +40,15 @@ def generate_frequency_plot(df, file_name=None):
 def generate_duration_plot(df, file_name=None):
     df['category'] = numpy.random.uniform(0, 1, len(df))
     fig, ax = plt.subplots()
-    ax.figure.set_size_inches(1, 3)
+    ax.figure.set_size_inches(PLOT_W, PLOT_H)
 
     g = sns.swarmplot(x='category', y='Width_mean', data=df, dodge=True, palette='viridis', ax=ax)
     sns.despine(fig=None, ax=None, top=True, right=True, left=False, bottom=False, offset=None, trim=False)
 
     ax.set_xlabel('')
     g.set(xticks=[])
-    ax.set_ylabel(r'Time ($\mu$s)', fontsize = 18)
-    plt.title("Duration", fontsize=18)
+    ax.set_ylabel(r'Time ($\mu$s)', fontsize = PLOT_LABEL_FONT_S)
+    plt.title("Duration", fontsize=PLOT_LABEL_FONT_S)
     if file_name is not None:
         plt.savefig(file_name)
     else:
@@ -55,15 +60,15 @@ def generate_area_plot(df, file_name = None):
 
     df['category'] = numpy.random.uniform(0, 1, len(df))
     fig, ax = plt.subplots()
-    ax.figure.set_size_inches(1, 3)
+    ax.figure.set_size_inches(PLOT_W, PLOT_H)
 
     g = sns.swarmplot(x='category', y='Area_mean', data=df, dodge=True, palette='viridis', ax=ax)
     sns.despine(fig=None, ax=None, top=True, right=True, left=False, bottom=False, offset=None, trim=False)
 
     ax.set_xlabel('')
     g.set(xticks=[])
-    ax.set_ylabel(r'Area ($\mu$m*s)', fontsize = 18)
-    plt.title("Area", fontsize=18)
+    ax.set_ylabel(r'Area ($\mu$m*s)', fontsize = PLOT_TITLE_FONT_S)
+    plt.title("Area", fontsize=PLOT_LABEL_FONT_S)
     if file_name is not None:
         plt.savefig(file_name)
     else:
@@ -72,15 +77,15 @@ def generate_area_plot(df, file_name = None):
 def generate_interval_plot(df, file_name):
     df['category'] = numpy.random.uniform(0, 1, len(df))
     fig, ax = plt.subplots()
-    ax.figure.set_size_inches(1, 3)
+    ax.figure.set_size_inches(PLOT_W, PLOT_H)
 
     g = sns.swarmplot(x='category', y='Interval_mean', data=df, dodge=True, palette='viridis', ax=ax)
     sns.despine(fig=None, ax=None, top=True, right=True, left=False, bottom=False, offset=None, trim=False)
 
     ax.set_xlabel('')
     g.set(xticks=[])
-    ax.set_ylabel(r'Distance \n' + r'$(mu*s)$', fontsize = 18)
-    plt.title("Spatial spread", fontsize=18)
+    ax.set_ylabel(r'Distance \n' + r'$(mu*s)$', fontsize = PLOT_LABEL_FONT_S)
+    plt.title("Spatial spread", fontsize=PLOT_TITLE_FONT_S)
     if file_name is not None:
         plt.savefig(file_name)
     else:
